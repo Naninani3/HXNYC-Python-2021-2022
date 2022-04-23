dc372eaaac1c3920112f6a73f10482722813003c
Naninani3 <90726502+Naninani3@users.noreply.github.com>
2022-04-23 12:44:44 -0400

    Create HW_week-03.py

---
+++ HW_week-03.py
@@ -0,0 +1,52 @@
+'''
+Complete chatbot. You may introduce yourself to the computer, including the following information: 
+your name, favorite sports, favorite food and much more !!Use your imagination..:)
+'''
+
+print('+sport+')
+
+print("hahahhahhhhhhhhh")
+
+Name=input('What is your name?') print('Nice to meet you, '+Name+'!')
+
+print('Nice to meet you, '+Name+'!')
+
+Name=input('What is your name?') # Assign an input to an variable. This input by default is a str
+print('Nice to meet you, ' + Name + '!')
+
+Sport=input('What is your favorite sport?') # Assign an input to an variable. This input by default is a str
+print(Sport + ' is fun')
+
+
+
+food=input('What is your favorite food?') # Assign an input to an variable. This input by default is a str
+print('tasty ' + food)
+
+head=input('How large is your head?') # remember to space in string, space around operator is optional
+print('your so called ' + head + ' head is definetly not ' +head)
+
+minute='360'
+minute_int=int(minute)
+print(minute_int)
+
+#1
+minutes=input('how many hours do you want to convert into minutes?')
+#2
+minutes=int(minutes)
+#3
+hour=minutes/60
+#4
+print(hour)
+
+abc=input('how many pounds do you want to convert into kilograms? ')
+abc_int=int(abc)
+xyz=abc_int/2.2046
+print(xyz)
+print(abc+ ' pounds = ' +str(xyz)+ ' kilograms')
+
+abc=input('how many minutes do you want to convert into seconds? ')
+abc_int=int(abc)
+xyz=abc_int*60
+print(xyz)
+print(abc+ ' minutes = ' +str(xyz)+ ' seconds')
+
