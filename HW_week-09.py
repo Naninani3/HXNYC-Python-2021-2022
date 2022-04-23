e02b4010d52716cbefb78072d28100ce120011a0
Naninani3 <90726502+Naninani3@users.noreply.github.com>
2022-04-23 14:26:59 -0400

    Create HW_week-09.py

---
+++ HW_week-09.py
@@ -0,0 +1,6 @@
+'''
+Write a function to draw turtle flowers or turtle pies. Can you make it more general? 
+Please follow the development plan if possible.
+'''
+
+https://trinket.io/python/a5a366cef8
