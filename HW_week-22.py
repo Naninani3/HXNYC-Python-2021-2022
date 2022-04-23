60ad6964bb3c6ea8f4172477738854fb8d54bec2
Naninani3 <90726502+Naninani3@users.noreply.github.com>
2022-04-23 15:08:07 -0400

    Create HW_week-22.py

---
+++ HW_week-22.py
@@ -0,0 +1,51 @@
+'''
+Understand how to use append, pop,sort, extend, remove, insert
+
+start from a list: l=[2,3,5,3,7,5,2,3]
+1) Append number 10 to the end of the list.
+2) sort the list
+3) extend the list with [1,2,3]
+4) remove 3
+5) insert 9 to index 4
+6) sort the list one more time.
+'''
+
+l=[2,3,5,3,7,5,2,3]
+
+#1) Append number 10 to the end of the list.
+
+l.append(10)
+
+print(l)
+
+#2) sort the list
+
+l.sort()
+
+print(l)
+
+#3) extend the list with [1,2,3]
+
+l.extend([1,2,3])
+
+print(l)
+
+#4) remove 3
+
+l.pop()
+
+print(l)
+
+#5) insert 9 to index 4
+
+l.insert(4,9)
+
+print(l)
+
+#6) sort the list one more time.
+
+l.sort()
+
+print(l)
+
+ 
