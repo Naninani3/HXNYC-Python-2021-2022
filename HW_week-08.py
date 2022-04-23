f48005690dd06ebb7aecb656995e7c3439db393f
Naninani3 <90726502+Naninani3@users.noreply.github.com>
2022-04-23 14:25:17 -0400

    Update HW_week-08.py

--- HW_week-08.py
+++ HW_week-08.py
@@ -3,3 +3,4 @@ Can you write a code to use turtle to draw a Hexagon?
 Please do use the concepts we learned in this class (Encapsulation, generalization and for loop)
 '''
 
+https://trinket.io/python/36af6390f4
