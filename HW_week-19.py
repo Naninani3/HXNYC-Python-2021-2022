672a457216aa255a5809ae4f459d56e8f0bfb6d5
Naninani3 <90726502+Naninani3@users.noreply.github.com>
2022-04-23 15:03:50 -0400

    Create HW_week-19.py

---
+++ HW_week-19.py
@@ -0,0 +1,11 @@
+'''
+Write a function, take a list and item, return how many times this item shows up in this list.==>Which is similar to our string homework :)
+'''
+
+def countlist2(list,item):
+
+    return list.count(item)
+
+ 
+
+countlist2("ueifhnveiuwchnewfiochefn", "i")
